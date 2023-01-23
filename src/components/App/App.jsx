76a39ce08d6c4eb850df774e7464d479261b4749
Karl Kraemer <karlekraemer@gmail.com>
2023-01-23 15:36:53 -0600

    routes connected. Get requests for pizzas and orders done, need to get the line item GET running too. POST request working for the order. Now to wait to learn about APIs in this week's classes in order to continue the project

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,9 +1,107 @@
 import React from 'react';
 import axios from 'axios';
+import { useEffect, useState } from 'react';
 import './App.css';
+import PizzaList from '../PizzaList/PizzaList';
 
 function App() {
+  
+  const [pizzaList, setPizzaList] = useState([]);
+  const [newCustomer_Name, setNewCustomer_Name] = useState('');
+  const [newStreet_Address, setNewStreet_Address] = useState('');
+  const [newCity, setNewCity] = useState('');
+  const [newType, setNewType] = useState('');
+  const [newTotal, setNewTotal] = useState('');
+  const [newPizzas, setNewPizzas] = useState('');
 
+
+  // GET pizza request
+  const fetchPizzas = () => {
+    axios({
+      method: 'GET',
+      url: '/api/pizza'
+    })
+      .then( (response) => {
+        console.log('Entire response:', response);
+        // The actual array comes from the data attribute on the response
+        console.log('Just the data:', response.data);
+
+        // Set data into component state
+        setPizzaList(response.data);
+      })
+      .catch(function (error) {
+        console.log('Error on get:', error);
+      });
+  }
+
+  // GET order reqeust
+  const fetchOrder = () => {
+    axios({
+      method: 'GET',
+      url: '/api/order'
+    })
+      .then( (response) => {
+        console.log('Entire response:', response);
+        // The actual array comes from the data attribute on the response
+        console.log('Just the data:', response.data);
+        
+        // Set data into component state
+        setPizzaList(response.data);
+      })
+      .catch(function (error) {
+        console.log('Error on get:', error);
+      });
+    }
+    
+////////////Need a GET request for the line item///////////
+    
+  // order form post request below
+  const submitOrder = (newCustomer_Name, newStreet_Address, newCity, newZip, newType, newTotal, newPizzas ) => {
+    axios({
+      method: 'POST',
+      url: '/api/order',
+      data: {
+        customer_name: newCustomer_Name,
+        street_address: newStreet_Address,
+        city: newCity,
+        zip: newZip,
+        type: newType,
+        total: newTotal,
+        pizzas: newPizzas // we can make this an array for multiple pizzas in stretch mode
+
+        //Fill out more here as we know what our order form looks like
+        
+      }
+    })
+      .then( (response) => {
+        console.log('Response:', response);
+        //fire GET request
+        fetchOrder();
+        //reset input fields
+        setNewCustomer_Name('');
+        setNewStreet_Address('');
+        setNewCity('');
+        setNewZip('');
+        setNewType('');
+        setNewTotal('');
+        setNewPizzas('');
+      })
+      .catch(function (error) {
+        console.log('Error on add:', error);
+      });
+  }
+  
+  useEffect(() => {
+    fetchPizzas();
+  }, []);
+
+  useEffect(() => {
+    fetchOrder();
+  }, []);
+   
+
+  
+  
   return (
     <div className='App'>
       <header className='App-header'>
@@ -12,7 +110,12 @@ function App() {
   
       <img src='images/pizza_photo.png' />
       <p>Pizza is great.</p>
-  
+      {/* <div className="PizzaList">
+        <ul>
+          <li>{pizzaList}</li>
+        </ul>
+      </div> */}
+  <PizzaList pizzaList = {pizzaList}/>
     </div>
   );
 }
---
+++ src/components/PizzaList/PizzaList.jsx
@@ -0,0 +1,10 @@
+function PizzaList(pizzaList) {
+
+    return (
+        <>
+        hi is this working
+        </>
+    )
+}
+
+export default PizzaList;
